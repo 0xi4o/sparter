643be3ae51b4ff1406570a63e33da5b4b4c8722b
Ilango <rajagopalilango@gmail.com>
2021-09-15 11:00:24 +0530

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
 	render() {
@@ -19,20 +20,20 @@ class MyDocument extends Document {
 						rel='stylesheet'
 					/>
 					{/* -- Global site tag (gtag.js) - Google Analytics --> */}
-					<script
-						async
-						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+					<Script
+						strategy='lazyOnload'
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
 					/>
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments)}
-						gtag('js', new Date());
-						gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
-						`,
-						}}
-					/>
+					<Script strategy='lazyOnload'>
+						{`
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments)}
+							gtag('js', new Date());
+							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+								page_path: window.location.pathname,
+							});
+						`}
+					</Script>
 				</Head>
 				<body className='bg-white dark:bg-gray-1000'>
 					<Main />
