7194e4f3029b99aade71935dc4cec8a4034b38f8
Ilango <rajagopalilango@gmail.com>
2021-09-15 10:49:27 +0530

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
+
 module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
@@ -12,10 +18,11 @@ module.exports = {
 		ecmaVersion: 12,
 		sourceType: 'module',
 	},
-	plugins: ['react'],
+	plugins: ['react', '@typescript-eslint'],
 	rules: {
 		'react/prop-types': 'off',
 		'react/react-in-jsx-scope': 'off',
+		'react/display-name': 'off',
 	},
 	settings: {
 		react: {
